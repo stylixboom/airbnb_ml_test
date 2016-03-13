4d6b74aae115b6d06d00c620648a172fe7600c75
Siriwat K <siriwat@live.jp>
2016-03-13 22:48:28 +0900

    Preprocessed train/test data
    Basic features only within train/test data were saved to files

--- prediction.py
+++ prediction.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
+import os.path
 
 """
 The dataset can be downloaded from here
@@ -16,6 +17,11 @@ SESSSIONS_CSV_FILE      = "./data/sessions.csv"
 TEST_CSV_FILE           = "./data/test_users.csv"
 TRAIN_CSV_FILE          = "./data/train_users_2.csv"
 RESULT_CSV_FILE         = "./data/result.csv"
+
+# Feature file
+TEST_FEAT_CSV_FILE      = "./data/test_users_feat.csv"
+TRAIN_FEAT_CSV_FILE     = "./data/train_users_feat.csv"
+LABEL_CSV_FILE          = "./data/label.csv"
 
 # Date formatting
 DATE_FORMAT = '%Y-%m-%d'
@@ -59,14 +65,14 @@ def filter_list(str_set, exclude_words):
     :return: Filter list
     """
     new_list = []
-    for str in str_set:
+    for val in str_set:
         contain_excluded = False
         for word in exclude_words:
-            if word in str:
+            if word in val:
                 contain_excluded = True
                 break
         if not contain_excluded:
-            new_list.append(str)
+            new_list.append(val)
     return new_list
 
 
@@ -90,45 +96,83 @@ def main():
     """
     Execute a prediction of AirBnb new user booking challenge.
 
-    1. Data pre-processing
+    1. Data pre-processing / Feature preparation
     2. Feature extraction
     3. Training
     4. Predicting
     """
+    # Variables
+    train_users = None
+    test_users = None
+    labels = None
 
-    # Load datasets
-    train_users = pd.read_csv(TRAIN_CSV_FILE, index_col=0)  # user_id is at col 0
-    test_users = pd.read_csv(TEST_CSV_FILE, index_col=0)    # user_id is at col 0
-    #sessions = pd.read_csv(SESSSIONS_CSV_FILE)
+    # Check if pre-processed data exist
+    if not (os.path.isfile(TRAIN_FEAT_CSV_FILE) & \
+            os.path.isfile(TEST_FEAT_CSV_FILE) & \
+            os.path.isfile(LABEL_CSV_FILE)):
 
-# 1. Data pre-processing
+        # Load datasets
+        train_users = pd.read_csv(TRAIN_CSV_FILE, index_col=0)  # user_id is at col 0
+        test_users = pd.read_csv(TEST_CSV_FILE, index_col=0)    # user_id is at col 0
+        #sessions = pd.read_csv(SESSSIONS_CSV_FILE, index_col=0) # user_id is at col 0
 
-    # Merge dataset for bigger training set
-    users = pd.concat((train_users, test_users), axis=0, ignore_index=True)     # ignore user_id index
+    # 1. Data pre-processing
 
-    # Extract/replacing datetime string to year,month,day,weekday for better feature
-    extract_date(users, 'date_account_created')
+        # Merge dataset for processing once together
+        users = pd.concat((train_users, test_users), axis=0)
 
-    # Drop non-necessary/not possible field to be predicted
-    users.drop('date_first_booking', inplace=True, axis=1)      # This field is not available on test set
-    users.drop('timestamp_first_active', inplace=True, axis=1)  # This field is quite duplicated to 'date_account_created'
-    users.drop('country_destination', inplace=True, axis=1)     # Predicting label
+        # Extract/replacing datetime string to year,month,day,weekday for better feature
+        extract_date(users, 'date_account_created')
 
-    # Cleaning noisy age
-    users.loc[users['age'] > 95, 'age'] = np.nan
-    users.loc[users['age'] < 13, 'age'] = np.nan
+        # Drop non-necessary/not possible field to be predicted
+        label_attrib = 'country_destination'            # Predicting label
+        drop_attribs = ['date_first_booking',           # This field is not available on test set
+                        'timestamp_first_active',       # This field is quite duplicated to 'date_account_created'
+                        label_attrib]
+        users.drop(drop_attribs, inplace=True, axis=1)
 
-    # Cleaning -unknown- age means nothing
-    users['gender'].replace('-unknown-', np.nan, inplace=True)
+        # Cleaning noisy age
+        users.loc[users['age'] > 95, 'age'] = -1
+        users.loc[users['age'] < 13, 'age'] = -1
+        users['age'].fillna(-2, inplace=True)
 
-    # Collect categorical attribute
-    categorical_features = filter_list(list(users.columns), ('date','time'))
+        # Cleaning first_affiliate_tracked
+        users['first_affiliate_tracked'].fillna('N/A', inplace=True)
 
-    category_to_one_hot_encoding(users,['gender'])
+        # Collect categorical attribute
+        categorical_features = filter_list(list(users.columns), ('date','time'))
 
+    # 2. Feature extraction
 
+        # Apply one-hot-encoding feature
+        users = category_to_one_hot_encoding(users, categorical_features)
 
+        # Keep groundtruth label
+        labels = train_users[label_attrib].copy()
+        train_users.drop(label_attrib, inplace=True, axis=1)
 
+        # Split back train/test users (with extracted features)
+        train_users = users.ix[train_users.index]
+        test_users = users.ix[test_users.index]
+
+        # Checking consistency before save
+        assert set(train_users.index) == set(labels.index)
+
+        # Save processed data
+        train_users.to_csv(TRAIN_FEAT_CSV_FILE, header=True)
+        test_users.to_csv(TEST_FEAT_CSV_FILE, header=True)
+        labels.to_csv(LABEL_CSV_FILE, header=True)
+    else:
+        # Load preprocessed data
+        train_users = pd.read_csv(TRAIN_FEAT_CSV_FILE, index_col=0)
+        test_users = pd.read_csv(TEST_FEAT_CSV_FILE, index_col=0)
+        labels = pd.read_csv(LABEL_CSV_FILE, index_col=0)
+
+        # Checking consistency after loaded
+        assert set(train_users.index) == set(labels.index)
+
+    # 3. Training
+    print("Training...")
 
 
 if __name__ == '__main__':
